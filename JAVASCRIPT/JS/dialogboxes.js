095f32eefa5a2e3408c29028734ad7a21050acee
vina <vinaneosoft@gmail.com>
2024-07-23 17:31:30 +0530

    commit

---
+++ JAVASCRIPT/JS/dialogboxes.js
@@ -0,0 +1,13 @@
+
+
+window.alert("Hurry! sale is ending soon");
+
+
+
+const answer=window.confirm("Do you really SUBMIT the exam");
+console.log(answer);
+
+
+const value=window.prompt("Enter your age to proceed", 18)
+console.log(value);
+console.log(typeof value);
