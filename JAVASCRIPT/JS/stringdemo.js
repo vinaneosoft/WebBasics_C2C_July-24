20fca0760cf227593c202f76b4f91fed66f3a992
vina <vinaneosoft@gmail.com>
2024-07-24 11:11:16 +0530

    commit

---
+++ JAVASCRIPT/JS/stringdemo.js
@@ -0,0 +1,7 @@
+let companyName="Neosoft";
+console.log(companyName);
+console.log(typeof companyName); // string
+
+let branch=new String("Rabale");
+console.log(branch);
+console.log(typeof branch); // object : String
